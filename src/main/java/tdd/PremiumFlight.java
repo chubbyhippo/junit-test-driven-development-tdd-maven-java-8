7b792f1452edbd31e8f01613db4c2a6ac87c1799
ch <mkmsacc@outlook.com>
2020-11-27 14:52:07 +0700

    Add PremiumFlightTests

---
+++ src/main/java/tdd/PremiumFlight.java
@@ -0,0 +1,22 @@
+package tdd;
+
+public class PremiumFlight extends Flight {
+
+	public PremiumFlight(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean addPassenger(Passenger passenger) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removePassenger(Passenger passenger) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
